a1fa807df4a9885945574cb0509c7f2f1a4cade2
Fiona Cai <69484076+fiona-cai@users.noreply.github.com>
2022-02-12 16:39:49 -0500

    Add files via upload

---
+++ articles.js
@@ -0,0 +1,52 @@
+var dataset = [
+    {
+      title: 'Attention - LAST DAY FOR ROSE & COOKIE SALES🚨🚨',
+      date: 'Feb 9 2022',
+      description:"If you are interested in purchasing a Rose or a Cookie for your friend, significant other, or even yourself - don\'t miss out on the last opportunity to place an order!  Where? Come visit the StuCo Table in the front foyer during the Lunch Period! \n Orders can be made through School Cash Online (SCOL). If you are unable to access your SCOL account, visit the StuCo table in the front foyer and a member will assist you.",
+    },
+    {
+      title: 'Black History Month',
+      date: 'Feb 10 2022',
+      description:"During Black History Month, we will profile notable Black Canadians who have helped shape Canadian heritage and identity, and who have made and continue to make enormous contributions to the wellbeing, and prosperity of our country.",
+    },
+    {
+      title: 'Bayview Yearbook',
+      date: 'Feb 10 2022',
+      description:"The Bayview Yearbook is now available for purchase on School Cash Online! Do not miss your only chance to purchase the yearbook containing your invaluable highschool memories that you will cherish forever. Remember, money cannot buy back time, so this is your perfect chance to acquire a time capsule of this unique school year. The yearbook is now on sale on School Cash Online until March. Quantities may be limited, so do not wait!",
+    },
+    {
+      title: 'Yearbook: Club Photo Submission',
+      date: 'Feb 11 2022',
+      description:"If you are interested in purchasing a Rose or a Cookie for your friend, significant other, or even yourself - don\'t miss out on the last opportunity to place an order!  Where? Come visit the StuCo Table in the front foyer during the Lunch Period! \n Orders can be made through School Cash Online (SCOL). If you are unable to access your SCOL account, visit the StuCo table in the front foyer and a member will assist you.",
+    },
+    {
+      title: 'Semester 2 timetable changes',
+      date: 'Feb 11 2022',
+      description:"Today is the last day to request a semester 2 timetable change. Be sure to fill out the form today as it will close at 3:00 pm. Guidance will continue to work on the course changes next week until we have finished all requests. Guidance will also be working on clearing the waitlist as well. Please be patient while we finish up all the request changes. Next week, guidance appointments are available ONLY for course selection and pathways planning. If you have booked an appointment to discuss semester 2 classes, please cancel it as all course changes are done through the google form. Course selection for the next school year is due by Feb. 24th so please make sure you are on My Pathway Planner making your selections for the 2022-2023 school year. Make an appointment with your counsellor if you need support.",
+    },
+    {
+      title: 'Anonymous Act of Kindness',
+      date: 'Feb 11 2022',
+      description:"Send your love in through the google form found on your grade google classroom: https://docs.google.com/forms/d/e/1FAIpQLSfFs-7b12tyWbv5v6OkRADOdIplTGgRSS0wnAMtCzy0ju2A-g/viewform",
+    },
+    {
+      title: 'Math Tutors',
+      date: 'Feb 11 2022',
+      description:"The math department would like to thank everyone who participated as a mathematics tutor last semester. If you are interested in signing up to help for this semester then please come down to the G wing. New sign up information is posted near the math offices G02 and G10.",
+    },
+    {
+      title: 'Blind Date with a Book',
+      date: 'Feb 11 2022',
+      description:"Hi Bengals! The Library Learning Commons is excited to launch our Blind Date with a Book Valentine's event today. Come meet the new love of your reading life",
+    },
+    {
+      title: 'SHSM',
+      date: 'Feb 11 2022',
+      description:"Grade 10s-- are you stressing about your lack of direction in life? Perhaps you have an idea of what you’d like to do, but need something to help show off your skills? Look no further than Bayview’s SHSM program, guaranteed to give you work experience, free certifications, and events galore! Forging your path starts with your arrival at our informational meeting today, at 4:30pm on Zoom. To get the link, join the Pre-SHSM classroom: the code is iffmhzq. See you there!",
+    },
+    {
+      title: 'Art Club Executive Applications',
+      date: 'Feb 11 2022',
+      description:"Hi Everyone! Applications for the BSS Art Club Executive Team are now open! If you are passionate about the arts, have an eye for design and have superior leadership skills then this application is for you! Please submit the application found on your grade Google Classroom by Thursday February 17th. Successful applicants will be contacted for an interview. https://bit.ly/bssartclub",
+    },
+];
